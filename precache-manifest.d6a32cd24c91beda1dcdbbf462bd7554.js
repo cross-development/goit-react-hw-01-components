02896d79ade5578ed3ddc9d6288081809ba402c7
Vitaliy Cross <derdavitaliy@gmail.com>
2020-06-21 09:18:26 +0300

    Updates

---
+++ precache-manifest.d6a32cd24c91beda1dcdbbf462bd7554.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d24027a7135c53ba1205a495fd0c5d42",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "8664279c2321237593b5",
+    "url": "/goit-react-hw-01-components/static/css/main.cb1d0694.chunk.css"
+  },
+  {
+    "revision": "c572ef6447e698d10bec",
+    "url": "/goit-react-hw-01-components/static/js/2.cb513923.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.cb513923.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8664279c2321237593b5",
+    "url": "/goit-react-hw-01-components/static/js/main.1043c0ee.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
