b012e5bbcdf63fbaf678df06a75a2d92ceeba822
Vitaliy Derda <derdavitaliy@gmail.com>
2020-07-20 22:17:47 +0300

    added react hw-01 components

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,22 +1,23 @@
 //Core
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const Section = ({ title, children }) => {
-  return (
-    <section>
-      {title && <h2>{title}</h2>}
-      {children}
-    </section>
-  );
+	return (
+		<section>
+			{title && <h2>{title}</h2>}
+			{children}
+		</section>
+	);
 };
 
 Section.defaultProps = {
-  title: "",
+	title: '',
 };
 
 Section.propTypes = {
-  title: PropTypes.string,
+	title: PropTypes.string,
+	children: PropTypes.node.isRequired,
 };
 
 export default Section;
